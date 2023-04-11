7e6da923b4e964d64b9bc65220a40bc7d32a4e52
DaJour Christophe <74025275+DaJourJChristophe@users.noreply.github.com>
2023-04-11 04:17:17 -0700

    Implement an atof function that kinda works (sometimes) .. :P
    This atof function does not accurately represent a floating point number 100% of the time.

--- src/atof.c
+++ src/atof.c
@@ -5,117 +5,126 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void parse_float(const char *s)
+static void parse_float(const char **s, float *y)
 {
   int r = 0;
   int n = 1;
   int x;
-  float y;
 
-  for (; *s; s++)
+  for (; **s; *s += 1)
   {
-    if (*s != 0x20)
+    if (**s != 0x20)
     {
       break;
     }
   }
 
-  if (*s == 0)
+  if (**s == 0)
   {
-    y = n * r;
-    printf("%f\n", y);
+    *y = n * r;
     return;
   }
 
-  if (*s == 0x2d)
+  if (**s == 0x2d)
   {
     n = (-1);
-    s++;
+    *s+=1;
   }
-  else if (*s == 0x2b)
+  else if (**s == 0x2b)
   {
     n = 1;
-    s++;
+    *s+=1;
   }
 
-  for (; *s && *s != '.'; s++)
+  for (; **s && **s != '.'; *s+=1)
   {
-    if (ae_match(*s, AE_NO_DIGIT))
+    if (ae_match(**s, AE_NO_DIGIT))
     {
       break;
     }
 
-    x = *s - 0x30;
+    x = **s - 0x30;
 
     if ((r > (INT_MAX / 10) || (r == (INT_MAX / 10) && x >  7)) ||
         (r < (INT_MIN / 10) || (r == (INT_MIN / 10) && x < -8)))
     {
-      y = (n < 1) ? INT_MIN : INT_MAX;
-      printf("%f\n", y);
+      *y = (n < 1) ? INT_MIN : INT_MAX;
       return;
     }
 
     r = 10 * r + x;
   }
 
-  y = n * r;
+  *y = n * r;
 
-  if (*s == '.')
+  if (**s == '.')
   {
-    s++;
+    *s+=1;
   }
 
   r = 0;
   n = 1;
 
-  for (; *s; s++)
+  for (; **s; *s+=1)
   {
-    if (*s != 0x20)
+    if (**s != 0x20)
     {
       break;
     }
   }
 
-  if (*s == 0)
+  if (**s == 0)
   {
-    y += 1 / (n * r);
-    printf("%f\n", y);
+    float f = n * r;
+    while (f >= 1)
+    {
+      f /= 10;
+    }
+    *y += f;
     return;
   }
 
-  if (*s == 0x2d)
+  if (**s == 0x2d)
   {
     n = (-1);
-    s++;
+    *s+=1;
   }
-  else if (*s == 0x2b)
+  else if (**s == 0x2b)
   {
     n = 1;
-    s++;
+    *s+=1;
   }
 
-  for (; *s; s++)
+  for (; **s; *s+=1)
   {
-    if (ae_match(*s, AE_NO_DIGIT))
+    if (ae_match(**s, AE_NO_DIGIT))
     {
       break;
     }
 
-    x = *s - 0x30;
+    x = **s - 0x30;
 
     if ((r > (INT_MAX / 10) || (r == (INT_MAX / 10) && x >  7)) ||
         (r < (INT_MIN / 10) || (r == (INT_MIN / 10) && x < -8)))
     {
-      y += 1 / (float)((n < 1) ? INT_MIN : INT_MAX);
-      printf("%f\n", y);
+      float f = (n < 1) ? INT_MIN : INT_MAX;
+      while (f >= 1)
+      {
+        f /= 10;
+      }
+      *y += f;
       return;
     }
 
     r = 10 * r + x;
   }
 
-  y += (float)(1 / (n * r));
-  printf("%f\n", y);
+  float f = n * r;
+  while (f >= 1)
+  {
+    f /= 10;
+  }
+  *y += f;
 }
 
 int main(void)
