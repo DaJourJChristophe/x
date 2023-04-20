d69ef17ac17a1cf4868a821c0055ee6899b70fdf
DaJourJChristophe <dc198jcn215n@gmail.com>
2023-04-19 22:56:17 -0700

    rewrite expression tree evaluator in c

--- src/eval.c
+++ src/eval.c
@@ -1,22 +1,172 @@
 #include "expr.h"
 
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * @brief Print a short-hand Expression for development purposes.
  */
 void print_expr(syntax_expression_t *expr);
 
-void eval(syntax_expression_t *root)
+/**
+ * @brief Define a collection of character tokens.
+ */
+enum
+{
+  /**
+   * @brief General Purpose Tokens.
+   */
+  DOT,
+  EOE,
+  LEXER_EOF,
+  EOL,
+  SPACE,
+  STAR,
+  SYMBOL,
+  TAB,
+  TEXT,
+  UNDEFINED,
+  WORD,
+
+  /**
+   * @brief Numeric Tokens.
+   */
+  DECIMAL,
+  NUMBER,
+
+  /**
+   * @brief Mathematical Operator Tokens.
+   */
+  ADDITION,
+  DIVISION,
+  EQUAL,
+  EXPONENTIAL,
+  MODULUS,
+  REMAINDER,
+  SUBTRACTION,
+
+  /**
+   * @brief Bitwise Operator Tokens.
+   */
+  BITWISE_AND,
+  BITWISE_OR,
+  BITWISE_TERNARY,
+  BITWISE_XOR,
+
+  /**
+   * @brief Conditional Tokens.
+   */
+  CONDITIONAL_AND,
+  CONDITIONAL_OR,
+
+  /**
+   * @brief Containment Tokens.
+   */
+  LEFT_CARET,
+  OPEN_CURLY_BRACKET,
+  OPEN_PARENTHESIS,
+  OPEN_SQUARE_BRACKET,
+  CLOSE_CURLY_BRACKET,
+  CLOSE_PARENTHESIS,
+  CLOSE_SQUARE_BRACKET,
+  RIGHT_CARET,
+
+  /**
+   * @brief Architecture Tokens.
+   */
+  LAMBDA,
+
+  /**
+   * @brief Comparator Tokens.
+   */
+  EQUALS,
+
+  /**
+   * @brief Iterator Tokens.
+   */
+  DECREMENT,
+  INCREMENT,
+
+  /**
+   * @brief Separator Tokens.
+   */
+  COLON,
+  COMMA,
+
+  /**
+   * @brief Special Tokens.
+   */
+  ANNOTATION,
+
+  /**
+   * @brief Reserved Word Tokens.
+   */
+  ABSTRACT,
+  BOOLEAN,
+  BREAK,
+  CASE,
+  CLASS,
+  CONST,
+  DEFAULT,
+  DOUBLE,
+  EXPORT,
+  FALSE,
+  FLOAT,
+  FOR,
+  IF,
+  IMMUTABLE,
+  IMPORT,
+  INTEGER,
+  IS,
+  MATRIX,
+  NIL,
+  OBJECT,
+  PACKAGE,
+  PRINT,
+  PRIVATE,
+  PROTECTED,
+  PUBLIC,
+  RETURN,
+  SCALAR,
+  SET,
+  STATIC,
+  STRING,
+  SWITCH,
+  TRUE,
+  UNLESS,
+  VECTOR,
+  VOID,
+  WHILE,
+  YIELD,
+};
+
+static int eval_calc(syntax_expression_t *root)
 {
   if (root == NULL)
   {
-    return;
+    return 0;
   }
 
-  eval(root->left);
+  if (root->left  == NULL &&
+      root->right == NULL)
+  {
+    return *(int *)root->data;
+  }
 
-  print_expr(root);
+  int leftEval  = eval_calc(root->left);
+  int rightEval = eval_calc(root->right);
 
-  eval(root->right);
+  switch (root->type)
+  {
+    case ADDITION:    return leftEval + rightEval;
+    case SUBTRACTION: return leftEval - rightEval;
+    case STAR:        return leftEval * rightEval;
+  }
+
+  return leftEval / rightEval;
+}
+
+void eval(syntax_expression_t *root)
+{
+  printf("%d\n", eval_calc(root));
 }
